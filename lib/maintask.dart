d39eab57766759329f5a6678eba28b1b62ceb395
Flugeeer <vkatashov16@gmail.com>
2025-10-19 01:12:03 +0300

    made Main task file

--- lib/maintask.dart
+++ lib/maintask.dart
@@ -1,2 +1,25 @@
 import "package:flutter/material.dart";
-import "package:taskmanagersystem/main.dart";
+//import "package:taskmanagersystem/main.dart";
+
+class MainTask extends StatefulWidget {
+  const MainTask({super.key, required this.title});
+  final String title;
+  @override
+  State<MainTask> createState() => _MainTaskState();
+}
+
+class _MainTaskState extends State<MainTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Container(
+      width: double.infinity,
+      child: Column(
+        
+
+      ),
+    ),  
+
+    );
+  }
+}
