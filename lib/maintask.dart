fd81089f3c5ebb2036f005c53c6c532d3db3aff9
Flugeeer <vkatashov16@gmail.com>
2025-11-02 01:18:51 +0300

    made a working page for main task which will show mini tasks to complete module

--- lib/maintask.dart
+++ lib/maintask.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:taskmanagersystem/main.dart";
 //import "package:taskmanagersystem/main.dart";
 
 class MainTask extends StatefulWidget {
@@ -12,14 +13,42 @@ class _MainTaskState extends State<MainTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Container(
-      width: double.infinity,
-      child: Column(
-        
-
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<void>(
+                        builder: (context) => const MyHomePage(title: "хуй"),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightGreen,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Вернуться на главный экран',
+                        style: TextStyle(color: Colors.amber),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),  
-
     );
   }
 }
