0c2bd537bd4477f3031efb3a68b58d0fcde172af
Flugeeer <vkatashov16@gmail.com>
2025-11-03 00:19:07 +0300

    changed imports

--- lib/pages/UserProfile.dart
+++ lib/pages/UserProfile.dart
@@ -1,8 +1,31 @@
 import "package:flutter/material.dart";
 import "../main.dart";
 import "../tasklist.dart";
+import "package:taskmanagersystem/maintaskpage.dart";
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => Scaffold();
+  _ProfilePage createState() => _ProfilePage();
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text('Profile')),
+    body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity, // Main Info of user,
+            height: 400,
+            color: Colors.lightGreen,
+          ),
+          Container(
+            width: double.infinity,
+            height: 300,
+            color: Colors.red,
+          ), // completed tasks and performance bar,
+        ],
+      ),
+    ),
+  );
 }
