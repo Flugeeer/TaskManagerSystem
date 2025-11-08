7e81a2b08615b3574ce8694789269ae76aa40c32
Flugeeer <vkatashov16@gmail.com>
2025-11-08 12:11:06 +0300

    Added login function to enter mainpage

--- lib/services/authservice.dart
+++ lib/services/authservice.dart
@@ -3,27 +3,46 @@ import 'package:taskmanagersystem/models/User.dart';
 
 class AuthService extends ChangeNotifier {
   User? _currentUser;
-}
 
-final List<User> _demoUsers = [
-  User(
-    id: '1',
-    email: 'flugeeer@tech.com',
-    password: '12345',
-    name: 'Vladislav',
-    lastname: 'Katashov',
-    expirience: 'Recruit',
-    Role: 'Recruit',
-    joinedAt: DateTime.now(),
-  ),
-  User(
-    id: '2',
-    email: '4lwayswannasleep@tech.ru',
-    password: '12345',
-    name: 'Vladislav',
-    lastname: 'Joslinn',
-    expirience: 'Senior',
-    Role: 'Backend',
-    joinedAt: DateTime.now(),
-  ),
-];
+  final List<User> _demoUsers = [
+    User(
+      id: '1',
+      email: 'flugeeer@tech.com',
+      password: '12345',
+      name: 'Vladislav',
+      lastname: 'Katashov',
+      expirience: 'Recruit',
+      Role: 'Recruit',
+      joinedAt: DateTime.now(),
+    ),
+    User(
+      id: '2',
+      email: '4lwayswannasleep@tech.ru',
+      password: '12345',
+      name: 'Vladislav',
+      lastname: 'Joslinn',
+      expirience: 'Senior',
+      Role: 'Backend',
+      joinedAt: DateTime.now(),
+    ),
+  ];
+  User? get currentUser => _currentUser;
+
+  bool login(String email, String password) {
+    try {
+      final user = _demoUsers.firstWhere(
+        (user) => user.email == email && user.password == password,
+      );
+      _currentUser = user;
+      notifyListeners();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  void _logout() {
+    _currentUser = null;
+    notifyListeners();
+  }
+}
